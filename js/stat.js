1505f1f2033963f455def4e95815e485c2dfe55d
Daria Baranova <dare.baranova@Mac-mini-Mihail.local>
2020-01-25 16:27:57 +0300

    Сделала функцию для вывода текста

--- js/stat.js
+++ js/stat.js
@@ -1,16 +1,15 @@
 'use strict';
 var CLOUD_WIDTH = 470;
 var CLOUD_HEIGHT = 270;
-var COLUM_WIDHT = 40;
+var COLUM_WIDTH = 40;
 var GAP = 10;
 var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var TEXT_Y = 260;
-var REDUCTION = 50;
 var RECTANGLE_Y = 240;
-var INDENT_SIDE = CLOUD_X + REDUCTION;
-var TOTAL_WIDHT = REDUCTION + COLUM_WIDHT;
-var BASE_BAR_HEIGHT = CLOUD_HEIGHT - 2 * REDUCTION;
+var INDENT_SIDE = 150;
+var TOTAL_WIDTH = 90;
+var BASE_BAR_HEIGHT = 170;
 
 var renderCloud = function (ctx, x, y, color, width, height) {
   ctx.fillStyle = color;
@@ -34,29 +33,36 @@ var getRandomNumber = function () {
 var getRoundNumber = function (number) {
   return Math.round(number);
 };
-
-window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7' +
-    ')', CLOUD_WIDTH, CLOUD_HEIGHT);
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff', CLOUD_WIDTH, CLOUD_HEIGHT);
-  ctx.fillStyle = '#000';
-  ctx.font = '16px  PT Mono ';
-  ctx.fillText('Ура вы победили!', CLOUD_X + GAP, 40);
-  ctx.fillText('Список результатов:', CLOUD_X + GAP, 60);
-  var maxTime = getMaxElement(times);
+var renderText = function (ctx, text, x, y, color, font) {
+  ctx.font = font;
+  ctx.fillStyle = color;
+  ctx.fillText(text, x, y);
+};
+var renderColor = function (ctx, names) {
   for (var i = 0; i < names.length; i++) {
     if (names[i] === 'Вы') {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
       ctx.fillStyle = 'hsl(255, 100%, ' + getRandomNumber() + '%)';
     }
-    var textNumber = getRoundNumber(times[i]);
-    var x = INDENT_SIDE + TOTAL_WIDHT * i;
-    var barHeight = -((BASE_BAR_HEIGHT * times[i]) / maxTime) + GAP * 2;
-    ctx.fillText(textNumber, x, TEXT_Y + barHeight);
-    ctx.fillRect(x, RECTANGLE_Y, COLUM_WIDHT, barHeight);
-    ctx.fillStyle = 'rgba(0, 0, 0, 1)';
-    ctx.fillText(names[i], x, TEXT_Y);
-    ctx.fillText(getRoundNumber(times[i]), x, RECTANGLE_Y + barHeight - GAP);
   }
 };
+var renderColumn = function (ctx, color, x, y, width, height) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, width, height);
+};
+window.renderStatistics = function (ctx, names, times) {
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7' +
+    ')', CLOUD_WIDTH, CLOUD_HEIGHT);
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff', CLOUD_WIDTH, CLOUD_HEIGHT);
+  renderText(ctx, 'Ура Вы победили', CLOUD_X + GAP, 40, '#000', '16px PT Mono');
+  renderText(ctx, 'Список результатов:', CLOUD_X + GAP, 60, '#000', '16px PT Mono');
+  var maxTime = getMaxElement(times);
+  var x = INDENT_SIDE + TOTAL_WIDTH * i;
+  var barHeight = -((BASE_BAR_HEIGHT * times[i]) / maxTime) + GAP * 2;
+  renderColumn(ctx, renderColor, x, RECTANGLE_Y, COLUM_WIDTH, barHeight);
+  ctx.fillStyle = 'rgba(0, 0, 0, 1)';
+  ctx.fillText(names[i], x, TEXT_Y);
+  ctx.fillText(getRoundNumber(times[i]), x, RECTANGLE_Y + barHeight - GAP);
+};
+
