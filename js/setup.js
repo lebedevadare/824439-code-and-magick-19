0c9f12f58700b23c76048f9712375809dbd58b54
Daria Baranova <dare.baranova@Mac-mini-Mihail.local>
2020-02-13 14:49:52 +0300

    Удалила ненужную переменную, изменила названия переменных

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,7 @@
 'use strict';
 var WIZARD_COUNT = 4;
+var ESC_KEY = 'Escape';
+var ENTER_KEY = 'Enter';
 var userDialog = document.querySelector('.setup');
 var userDialogSimilar = userDialog.querySelector('.setup-similar');
 var similarListElement = userDialogSimilar.querySelector('.setup-similar-list');
@@ -9,14 +11,13 @@ var wizardSurnames = ['да Марья', 'Верон', 'Мирабелла', 'В
 var eyeColors = ['black', 'red', 'blue', 'yellow', 'green'];
 var coatColors = ['101, 137, 164', '241, 43, 107', '146, 100, 161', '56, 159, 117', '215, 210, 55', '0, 0, 0'];
 var wizards = [];
-var firewall = ['#ee4830', '#30a8ee', '#5ce6c0', '#5ce6c0', '#e848d5', '#e6e848'];
-var setup = document.querySelector('.setup');
+var fireballColors = ['#ee4830', '#30a8ee', '#5ce6c0', '#5ce6c0', '#e848d5', '#e6e848'];
 var setupOpen = document.querySelector('.setup-open');
 var setupClose = setup.querySelector('.setup-close');
+var playerWizard = document.querySelector('.setup-wizard');
 var wizardCoat = document.querySelector('.wizard-coat');
-var wizardEyeColor = document.querySelector('.wizard-eyes')
-var ESC_KEY = 'Escape';
-var ENTER_KEY = 'Enter';
+var wizardEye = document.querySelector('.wizard-eyes');
+
 
 var getRandomInteger = function (min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
@@ -80,16 +81,16 @@ var onPopupEscPress = function (evt) {
 };
 
 var openPopup = function () {
-  setup.classList.remove('hidden');
+  userDialog.classList.remove('hidden');
   document.addEventListener('keydown', onPopupEscPress);
 };
 
 var closePopup = function () {
-  setup.classList.add('hidden');
+  userDialog.classList.add('hidden');
   document.removeEventListener('keydown', onPopupEscPress);
 };
 var addCoatColors = function () {
-  document.querySelector('.wizard-coat').style.fill = getRandomArrayElement(coatColors);
+  wizardCoat.style.fill = getRandomCoatColor(coatColors);
 };
 
 setupOpen.addEventListener('click', function () {
@@ -116,6 +117,6 @@ wizardCoat.addEventListener('click', function () {
   addCoatColors();
 });
 
-wizardEyeColor.addEventListener('click', function () {
+wizardEye.addEventListener('click', function () {
 
 });
